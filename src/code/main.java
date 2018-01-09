70617af7a37ae28c8906b7998d544fb75e120c99
brauj1894 <brauj1894@FHC-IC0034106.wrdsb.ca>
2018-01-09 11:47:47 -0500

    Finished Battle Code

--- src/code/main.java
+++ src/code/main.java
@@ -1,19 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package code;
 
-/**
- *
- * @author brauj1894
- */
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Null Pointer Exception");
     }
 }
